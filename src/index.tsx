1cbe55a81761c335d66a7c01aa1bc2025d859277
Mark Lumbao <markanthonylumbao@gmail.com>
2020-05-02 23:57:58 +0800

    boilerplate impprovements.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-import './assets/tailwind.css';
+import App from 'components/app';
+import 'assets/tailwind.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
