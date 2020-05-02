1e4685228917ea72c3b6ea96643f99c4af185d24
Mark Lumbao <markanthonylumbao@gmail.com>
2020-05-02 23:59:39 +0800

    boilerplate improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,79 @@
-const { join, resolve } = require('path');
+const { join, resolve: pathResolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: join(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name][hash].bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.jsx'],
-    modules: [
-      'node_modules',
-      resolve(__dirname, 'src'),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'eslint-loader',
-        enforce: 'pre',
-      },
-      {
-        test: /\.js$/,
-        use: ['source-map-loader'],
-        enforce: 'pre',
-      },
-      {
-        test: /\.js$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(png|jpeg|jpg|svg|ico)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
+const entry = join(__dirname, '/src/index.tsx');
 
-          },
+const output = {
+  path: join(__dirname, 'dist'),
+  filename: '[name].bundle.js',
+  chunkFilename: '[name][hash].bundle.js',
+};
+
+const resolve = {
+  extensions: ['.js', '.ts', '.tsx', '.jsx'],
+  modules: [
+    'node_modules',
+    pathResolve(__dirname, 'src'),
+  ],
+};
+
+const loaderModules = {
+  rules: [
+    {
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: 'ts-loader',
+    },
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: 'eslint-loader',
+      enforce: 'pre',
+    },
+    {
+      test: /\.js$/,
+      use: ['source-map-loader'],
+      enforce: 'pre',
+    },
+    {
+      test: /\.js$/,
+      use: ['babel-loader'],
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'postcss-loader'],
+    },
+    {
+      test: /\.(png|jpeg|jpg|svg|ico)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+
         },
       },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/public/index.html',
-    }),
+    },
   ],
-  devServer: {
-    overlay: true,
-  },
+};
+
+const plugins = [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    template: './src/public/index.html',
+    minify: true,
+    favicon: 'src/assets/images/logo.png',
+  }),
+];
+
+const devServer = {
+  overlay: true,
+};
+
+module.exports = {
+  entry,
+  output,
+  resolve,
+  module: loaderModules,
+  plugins,
+  devServer,
 };
